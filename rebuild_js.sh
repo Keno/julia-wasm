c5539335ec6182ffbc3ba77e908561c2fc91dfd1
Keno Fischer <keno@juliacomputing.com>
2018-12-30 20:27:18 -0500

    Fix rebuild script

--- rebuild_js.sh
+++ rebuild_js.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $(DIR)/julia/build-wasm
-make -C src && cp ../build-native/usr/lib/julia/sys.ji . && emcc -Isrc/support -Lusr/lib -ljulia -lLLVMSupport ui/wasm-support.c --preload-file base/boot.jl --preload-file sys.ji --no-heap-copy --source-map-base http://localhost:8888/ -g4 -s -s WASM=1 -s ASSERTIONS=2 -s ALLOW_MEMORY_GROWTH=1 -s 'EXPORTED_FUNCTIONS=['_main','_jl_toplevel_eval_in']' -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s RESERVED_FUNCTION_POINTERS=20 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['stringToUTF8']" --emrun -o
-$(DIR)/website/hello.js -fcolor-diagnostics
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR/julia/build-wasm
+make -C src && cp ../build-native/usr/lib/julia/sys.ji . && emcc -DJL_DISABLE_LIBUV -Isrc -I../src -I../src/support -Lusr/lib -ljulia -lLLVMSupport ../ui/wasm-support.c --preload-file base/boot.jl --preload-file sys.ji --no-heap-copy --source-map-base http://localhost:8888/ -g4 -s -s WASM=1 -s ASSERTIONS=2 -s ALLOW_MEMORY_GROWTH=1 -s 'EXPORTED_FUNCTIONS=['_main','_jl_toplevel_eval_in']' -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s RESERVED_FUNCTION_POINTERS=20 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['stringToUTF8']" --emrun -o $DIR/website/hello.js -fcolor-diagnostics
