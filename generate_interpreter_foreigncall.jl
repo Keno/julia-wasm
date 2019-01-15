a0d7a00eeb99f2506fd3600dc0b0334eb1b2ad4c
Keno Fischer <keno@juliacomputing.com>
2019-01-15 00:21:55 -0500

    jl_weakref_t is also Any

--- generate_interpreter_foreigncall.jl
+++ generate_interpreter_foreigncall.jl
@@ -43,7 +43,7 @@ const CLSugared = Union{CLTypedef, CLElaborated}
 is_ptr(t::CLPointer) = true
 is_ptr(t::CLSugared) = is_ptr(unsugar(t))
 is_ptr(t) = false
-is_any(t::CLPointer) = spelling(pointee_type(t)) in ("jl_value_t", "jl_module_t", "jl_array_t", "jl_function_t", "jl_task_t")
+is_any(t::CLPointer) = spelling(pointee_type(t)) in ("jl_value_t", "jl_module_t", "jl_array_t", "jl_function_t", "jl_task_t", "jl_weakref_t")
 is_any(t) = false
 
 deptr(t::CLPointer) = pointee_type(t)
