c0d7212af6e472eec787aa7a4cbf72cf4c25ded3
Keno Fischer <keno@juliacomputing.com>
2019-07-04 18:15:47 -0400

    New julia branch

--- build_julia_wasm.sh
+++ build_julia_wasm.sh
@@ -2,7 +2,7 @@
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 git clone https://github.com/JuliaLang/julia julia
 cd julia
-git checkout kf/wasm3
+git checkout kf/wasm4
 make O=build-native configure
 make O=build-wasm configure
 cat > build-native/Make.user <<EOF
@@ -11,10 +11,13 @@ JULIA_THREADS := 0
 ARCH=i686
 BUILD_ARCH_OVERRIDE := wasm
 UNAME_OVERRIDE := Emscripten
+USE_BINARYBUILDER_LLVM := 0
 EOF
 cat > build-wasm/Make.user <<EOF
 override CC=emcc
 override CXX=emcc
+override AR=/home/keno/llvm-project-build/bin/llvm-ar
+override RANLIB=/home/keno/llvm-project-build/bin/llvm-ranlib
 JULIACODEGEN=none
 CFLAGS=--source-map-base http://localhost:8888/ -g4 -s WASM=1 -D__wasm__
 override OS=emscripten
@@ -29,10 +32,10 @@ override USE_CROSS_FLISP=1
 override DISABLE_LIBUV=1
 override JULIA_THREADS=0
 override XC_HOST=wasm32-unknown-emscripten
-USE_BINARYBUILDER_LLVM=1
+override USE_LLVM_SHLIB=0
+override fPIC=
+USE_BINARYBUILDER_LLVM=0
 BINARYBUILDER_TRIPLET=wasm32-unknown-emscripten
-override LLVM_BB_URL_BASE := https://github.com/tshort/LLVMBuilder/releases/download
-override LLVM_BB_REL = 0-wasm
 EOF
 (cd build-native && make -j20)
 (cd build-wasm && make -C deps BUILDING_HOST_TOOLS=1 install-libuv install-utf8proc)
