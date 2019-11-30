2b5feab603bfcc72ae50043298b9112631449e0e
Valentin Churavy <v.churavy@gmail.com>
2019-11-30 15:22:21 -0500

    try building wasm

--- build_julia_wasm.sh
+++ build_julia_wasm.sh
@@ -1,43 +1,12 @@
 #!/bin/bash
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-git clone https://github.com/JuliaLang/julia julia
-cd julia
-git checkout kf/wasm4
-make O=build-native configure
-make O=build-wasm configure
-cat > build-native/Make.user <<EOF
-DISABLE_LIBUV := 1
-JULIA_THREADS := 0
-ARCH=i686
-BUILD_ARCH_OVERRIDE := wasm
-UNAME_OVERRIDE := Emscripten
-USE_BINARYBUILDER_LLVM := 0
-EOF
-cat > build-wasm/Make.user <<EOF
-override CC=emcc
-override CXX=emcc
-override AR=llvm-ar
-override RANLIB=llvm-ranlib
-JULIACODEGEN=none
-CFLAGS=--source-map-base http://localhost:8888/ -g4 -s WASM=1 -D__wasm__
-override OS=emscripten
-override DISABLE_LIBUNWIND=1
-override JULIA_THREADS=0
-override USE_SYSTEM_BLAS=1
-override USE_SYSTEM_LAPACK=1
-override USE_SYSTEM_LIBM=1
-override USE_SYSTEM_SUITESPARSE=1
-override LLVM_CONFIG_HOST=${DIR}/julia/build-native/usr/tools/llvm-config
-override USE_CROSS_FLISP=1
-override DISABLE_LIBUV=1
-override JULIA_THREADS=0
-override XC_HOST=wasm32-unknown-emscripten
-override USE_LLVM_SHLIB=0
-override fPIC=
-USE_BINARYBUILDER_LLVM=0
-BINARYBUILDER_TRIPLET=wasm32-unknown-emscripten
-EOF
+
+export PATH="${DIR}/llvm-build/bin:${PATH}"
+source $DIR/emsdk/emsdk_env.sh
+
+pushd julia
+
 (cd build-wasm && make -C deps BUILDING_HOST_TOOLS=1 install-libuv install-utf8proc)
-(cd build-wasm && make -C deps -j20)
-(cd build-native && make -j20)
-(cd build-wasm && make -j20 julia-ui-release)
+(cd build-wasm && make -C deps -j)
+(cd build-native && make -j)
+(cd build-wasm && make -j julia-ui-release)
---
+++ configure_julia_wasm.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+git clone https://github.com/JuliaLang/julia julia
+pushd julia
+git checkout kf/wasm4
+make O=build-native configure
+make O=build-wasm configure
+cat > build-native/Make.user <<EOF
+DISABLE_LIBUV := 1
+JULIA_THREADS := 0
+ARCH=i686
+BUILD_ARCH_OVERRIDE := wasm
+UNAME_OVERRIDE := Emscripten
+USE_BINARYBUILDER_LLVM := 0
+JULIA_PRECOMPILE=0
+EOF
+cat > build-wasm/Make.user <<EOF
+override CC=emcc
+override CXX=emcc
+override AR=llvm-ar
+override RANLIB=llvm-ranlib
+JULIACODEGEN=none
+CFLAGS=--source-map-base http://localhost:8888/ -g4 -s WASM=1 -D__wasm__
+override OS=emscripten
+override DISABLE_LIBUNWIND=1
+override JULIA_THREADS=0
+override USE_SYSTEM_BLAS=1
+override USE_SYSTEM_LAPACK=1
+override USE_SYSTEM_LIBM=1
+override USE_SYSTEM_SUITESPARSE=1
+override LLVM_CONFIG_HOST=${DIR}/julia/build-native/usr/tools/llvm-config
+override USE_CROSS_FLISP=1
+override DISABLE_LIBUV=1
+override JULIA_THREADS=0
+override XC_HOST=wasm32-unknown-emscripten
+override USE_LLVM_SHLIB=0
+override fPIC=
+USE_BINARYBUILDER_LLVM=0
+BINARYBUILDER_TRIPLET=wasm32-unknown-emscripten
+EOF
